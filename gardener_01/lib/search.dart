15eb29c26c14164a38bfb0d6e68af2ab205a2a3c
kaho <kaho@kahos-iMac.local>
2019-08-22 17:11:49 +0800

    preparing the searching page
    123

---
+++ gardener_01/lib/search.dart
@@ -0,0 +1,4 @@
+//use ListTile and JSON https://www.youtube.com/watch?v=EwHMSxSWIvQ
+// for reference https://github.com/JideGuru/FlutterSocialAppUIKit
+//interesting license http://www.wtfpl.net/about/
+
