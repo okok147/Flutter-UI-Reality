ebb2788b30170ab03cea3f9e40e73b293ba6e26e
kaho <kaho@kahos-iMac.local>
2019-08-23 10:15:39 +0800

    day 2

--- gardener_01/lib/main.dart
+++ gardener_01/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        backgroundColor: Color(0xffE3F6FF),
+        //backgroundColor: Color(0xffE3F6FF),
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
           dragStartBehavior: DragStartBehavior.start,
           children: <Widget>[
             Container(
-              color: Color(0xffE3F6FF),
+              //color: Color(0xffE3F6FF),
               alignment: Alignment.topCenter,
               height: 450.0,
               width: 1000.0,
@@ -55,9 +55,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
-            Container(
-              height: 450.0,
-              color: Colors.white60,
+            new Container(
+              height: 500.0,
+              decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(20.0),
+                    topRight: const Radius.circular(20.0),
+                  )),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30.0, right: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Spacer(flex: 2),
+                      Container(
+                          height: 90.0,
+                          color: Colors.blue,
+                          width: 1000.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Find a'),
+                                Spacer(),
+                                Text('Gardener')
+                              ],
+                            ),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0),
+                        child:
+                            Container(height: 100.0, color: Colors.redAccent),
+                      ),
+                      Spacer(flex: 11),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ));
