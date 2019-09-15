493428faa3548d0c63df93bd701f296c45863c39
kaho <kaho@kahos-iMac.local>
2019-09-15 23:01:12 +0800

    hints how to make a transparent button with border

--- friendly_forest_02/lib/utils.dart
+++ friendly_forest_02/lib/utils.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:friendly_forest_02/views/home.dart';
+
 class SizeConfig {
   static MediaQueryData _mediaQueryData;
   static double screenWidth;
@@ -149,5 +150,4 @@ var dismissListView = ListView(
   ],
 );
 
-
-var pageIndex =  1;
+var pageIndex = 1;
