5500da0c074c7bfd6331b8561a777be2fb1a8122
Lau Ka Ho <kaho@Laus-MacBook-Pro.local>
2019-09-24 14:42:22 +0800

    update

--- market_app_03/ios/Flutter/flutter_export_environment.sh
+++ market_app_03/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kaho/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kaho/Documents/GitHub/Flutter_UI_Reality/market_app_04"
-export "FLUTTER_TARGET=/Users/kaho/Documents/GitHub/Flutter_UI_Reality/market_app_04/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kaho/Documents/GitHub/Flutter_UI_Reality/market_app_03"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kaho/Documents/flutter/bin/cache/artifacts/engine/ios"
