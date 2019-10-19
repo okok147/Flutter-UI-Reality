e8bc413abba2fdba841f7173c551eb2b0ff6e13d
kaho <kaho@kahos-iMac.local>
2019-10-19 11:35:00 +0800

    day2

--- mindfulness_05/lib/main.dart
+++ mindfulness_05/lib/main.dart
@@ -29,8 +29,58 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(child: Text('Mindfulness')),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(22, 0, 0, 0),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: screenHeight / 18,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    height: screenHeight / 19.5,
+                    width: screenWidth / 9,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.squarespace-cdn.com/content/v1/5a9aaafc9772ae0eafefb6d2/1522595461172-K0M62BZXB9D95K5GKTHR/ke17ZwdGBToddI8pDm48kOaD2e8dx9ABzbAyRI3ihwZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UQF4sn_BNjf_S0hsQ3YiA0G6sWp1W5TVWiZLjwta0rE9wG3dwlhpSXXV9Hp3gq5RQg/IMG_0001.JPG?format=500w'),
+                    ),
+                  ),
+                  Text(
+                    'Hi, Karen',
+                    style: TextStyle(
+                        fontSize: screenHeight / 54,
+                        color: Color(0xff261738),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    width: screenWidth / 2.4,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.sort),
+                    onPressed: () {},
+                    color: Color(0xff261738),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: screenHeight / 15,
+              width: screenWidth,
+              color: Colors.greenAccent.withOpacity(0.00),
+            ),
+            Container(
+              height: screenHeight / 1.2,
+              width: screenWidth,
+              color: Colors.blueAccent.withOpacity(0.00),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
