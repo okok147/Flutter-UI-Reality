688daeb8730420f4e7a895d6ddbe7d607fc6e975
OK <kelvin147789@gmail.com>
2019-11-14 20:51:48 +0800

    project 5

---
+++ files_management_app_05/android/app/src/main/kotlin/com/example/files_management_app_05/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.files_management_app_05
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
