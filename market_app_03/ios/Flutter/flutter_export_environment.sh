183083d7904f7b6d08bfa60c6028ed666b711894
kelvin lau <kelvin@ibasezero.com>
2019-12-03 17:08:03 +0800

    update

--- market_app_03/ios/Flutter/flutter_export_environment.sh
+++ market_app_03/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ok/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ok/Documents/Flutter-UI-Reality/market_app_03"
+export "FLUTTER_ROOT=/Users/kelvin/Documents/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kelvin/Documents/workspace/Flutter-UI-Reality/market_app_03"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ok/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kelvin/Documents/SDK/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
