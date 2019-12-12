bcb71730ec1391abf9557b2634ff9fcec8661161
kelvin lau <kelvin@ibasezero.com>
2019-12-12 18:30:36 +0800

    update

---
+++ widget_explore/jojobus_schedule/lib/TicketPage.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+
+// import 'package:pretty_qr_code/pretty_qr_code.dart';
+
+class ClipShadowPath extends StatelessWidget {
+  final Shadow shadow;
+  final CustomClipper<Path> clipper;
+  final Widget child;
+
+  ClipShadowPath({
+    @required this.shadow,
+    @required this.clipper,
+    @required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _ClipShadowShadowPainter(
+        clipper: this.clipper,
+        shadow: this.shadow,
+      ),
+      child: ClipPath(child: child, clipper: this.clipper),
+    );
+  }
+}
+
+class _ClipShadowShadowPainter extends CustomPainter {
+  final Shadow shadow;
+  final CustomClipper<Path> clipper;
+
+  _ClipShadowShadowPainter({@required this.shadow, @required this.clipper});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = shadow.toPaint();
+    var clipPath = clipper.getClip(size).shift(shadow.offset);
+    canvas.drawPath(clipPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+ 
+
+class TicketPage extends StatefulWidget {
+  @override
+  _TicketPageState createState() => _TicketPageState();
+}
+
+
+
+class _TicketPageState extends State<TicketPage> {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData(fontFamily: 'noto');
+    return  Stack(
+        children: <Widget>[
+          Image(
+            // hidden
+              // image: Images.bgMap,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height),
+          Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+              ),
+              body: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: ticket(context),
+                  ),
+                  //        Container(
+                  //   height: 40.0,
+                  //   width: MediaQuery.of(context).size.width,
+                  //   padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  //   child: RaisedButton(
+                  //     onPressed: () async {
+
+                  //     },
+                  //     color: Cor.mainColor,
+                  //     child: Text(
+                  //       "下一步",
+                  //       style: TextStyle(color: Colors.white, fontSize: 14.0),
+                  //     ),
+                  //   ),
+                  // )
+                ],
+              )
+              // body: ListView.builder(
+              //        itemBuilder: (_,i){
+              //         return ListTile(title: Text(i.toString()),);
+              //       },
+
+              // ),
+              // body: SingleChildScrollView(
+              //   child: ColumnBuilder(r
+              //       itemBuilder: (_,i){
+              //         return ListTile(title: Text(i.toString()),);
+              //       },
+              //       itemCount: 199,
+              //   )
+              // ),
+              ),
+        ],
+      );
+    
+  }
+
+  Widget ticket(BuildContext context) {
+    return ClipShadowPath(
+      shadow: Shadow(
+        color: Color(0xffCACACC),
+        blurRadius: 12.0,
+        offset: Offset(-2, 0),
+      ),
+      clipper: TicketClipper(),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 36.0, vertical: 24.0),
+        width: MediaQuery.of(context).size.width,
+        height: 540,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.0),
+          // boxShadow: [
+
+          // ]
+        ),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'ABC123',
+              style: TextStyle(fontSize: 24.0, color: Color(0xff5c5c5c)),
+            ),
+            SizedBox(height: 19.0),
+            Container(
+              child: PrettyQr(
+                // hidden
+                // image: Images.Bus,
+                typeNumber: 3,
+                size: 180,
+                data: 'https://www.google.com',
+                roundEdges: true,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              '*必須準時，需依指定時間',
+              style: TextStyle(color: Color(0xff919191), fontSize: 12.0),
+            ),
+            SizedBox(
+              height: 31.0,
+            ),
+            Container(
+              color: Color(0xffF0F0F0),
+              height: 1,
+              width: MediaQuery.of(context).size.width,
+            ),
+            SizedBox(
+              height: 31.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '西灣河',
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                          Text(
+                            '香港島',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Image(
+                    // hidden
+                    // image: Images.twowayarrow,
+                  ),
+                ),
+                // Container(
+                //   child: Column(children: <Widget>[
+                //    Icon(Icons.arrow_right),
+                //    Icon(Icons.arrow_left),
+
+                //   ],),
+                // ),
+
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 24.0),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            '旺角',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Color(0xFF3D3D3D),
+                              fontSize: 24.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: Text(
+                              '西九龍',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFF3D3D3D),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '出發日期:  周五,11月29日 2019.  20:00',
+                      style: TextStyle(
+                          color: Color(0xff3D3D3D),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      '回程日期:  周五,11月29日 2019.  20:00',
+                      style: TextStyle(
+                          color: Color(0xff3D3D3D),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      '*必須準時，需依指定時間',
+                      style:
+                          TextStyle(color: Color(0xff919191), fontSize: 12.0),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TicketClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    const height = 300.2;
+
+    Path path = Path();
+
+    path.lineTo(0.0, height);
+    path.lineTo(16.0, height + 16.0);
+    path.lineTo(0.0, height + 32.0);
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, height + 32);
+    path.lineTo(size.width - 16, height + 16);
+    path.lineTo(size.width, height);
+    path.lineTo(size.width, 0.0);
+
+    // path.addOval(Rect.fromCircle(
+    //     center: Offset(0.0, size.height / 2), radius: 20.0));
+    // path.addOval(Rect.fromCircle(
+    //     center: Offset(size.width, size.height / 2), radius: q20.0));
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
