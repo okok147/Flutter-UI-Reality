ab561f4f788aa9892c05361238be6348401d2f84
OK <kelvin147789@gmail.com>
2020-03-25 19:53:58 +0800

    add project 5

---
+++ mobile_app_misaki_05/android/app/src/main/kotlin/com/example/mobile_app_misaki_05/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobile_app_misaki_05
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
